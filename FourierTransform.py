91f1e8050eb51093fbebb6221af7e9388602f626
Aliaksei <alexx333by@protonmail.com>
2019-10-12 21:11:56 +0300

    Added restoring polyharmonic signal

--- FourierTransform.py
+++ FourierTransform.py
@@ -22,8 +22,8 @@ class DirectFourierTransformer:
                           self.get_sinus_component_amplitude(harmonic_number))
 
     def get_initial_phase(self, harmonic_number):
-        return math.atan(self.get_sinus_component_amplitude(harmonic_number)
-                         / self.get_cosine_component_amplitude(harmonic_number))
+        return math.atan2(self.get_sinus_component_amplitude(harmonic_number),
+                          self.get_cosine_component_amplitude(harmonic_number))
 
     def get_amplitude_spectrum(self):
         sequence_length = len(self.__sequence)
@@ -45,4 +45,13 @@ class InverseFourierTransformer:
         trigonometric_const_part = 2 * math.pi / self.__spectrum_length
         return [sum(self.__amplitude_spectrum[j] * math.cos(trigonometric_const_part * j * i - self.__phase_spectrum[j])
                     for j in terms_count_range)
+                for i in range(self.__spectrum_length)]
+
+    def restore_polyharmonic(self, consider_phase_spectrum=True):
+        phases = self.__phase_spectrum if consider_phase_spectrum else [0] * self.__spectrum_length
+        terms_count_range = range(1, self.__spectrum_length // 2)
+        trigonometric_const_part = 2 * math.pi / self.__spectrum_length
+        return [self.__amplitude_spectrum[0] / 2
+                + sum(self.__amplitude_spectrum[j] * math.cos(trigonometric_const_part * j * i - phases[j])
+                      for j in terms_count_range)
                 for i in range(self.__spectrum_length)]
