5542091e5c4ec9153929541d9fa4073c1e67a6dd
Aliaksei <alexx333by@protonmail.com>
2019-10-13 14:30:51 +0300

    Fixed filter

--- filter.py
+++ filter.py
@@ -6,15 +6,17 @@ Spectrum = namedtuple('Spectrum', ['amplitude', 'phase'])
 def filter_spectrum(spectrum, low_bound=None, high_bound=None):
     spectrum_length = min(len(spectrum.amplitude), len(spectrum.phase))
 
-    low = low_bound if low_bound is not None else 0
-    high = high_bound if high_bound is not None else spectrum_length
-    filtered_amplitude = [0.0] * low
-    filtered_phase = [0.0] * low
+    low = min(low_bound if low_bound is not None else 0, spectrum_length // 2)
+    high = min(high_bound if high_bound is not None else spectrum_length // 2, spectrum_length // 2)
+    filtered_amplitude = []
+    filtered_phase = []
 
-    for i in range(low, high):
-        filtered_amplitude.append(spectrum.amplitude[i])
-        filtered_phase.append(spectrum.phase[i])
+    def get_symmetric_index(index):
+        return index if index < spectrum_length // 2 else spectrum_length - index
 
-    filtered_amplitude += [0.0] * (spectrum_length - high)
-    filtered_phase += [0.0] * (spectrum_length - high)
+    for i in range(spectrum_length):
+        symmetric_i = get_symmetric_index(i)
+        filtered_amplitude.append(spectrum.amplitude[i] if low < symmetric_i < high else 0.0)
+        filtered_phase.append(spectrum.phase[i] if low < symmetric_i < high else 0.0)
+
     return Spectrum(filtered_amplitude, filtered_phase)
